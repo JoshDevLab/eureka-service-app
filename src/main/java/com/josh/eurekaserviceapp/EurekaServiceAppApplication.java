25cdcc2ea46db44b046c1e02d338aeb64ddb194a
josh <tkddns6007@gmail.com>
2023-07-03 18:30:10 +0900

    Initial commit

---
+++ src/main/java/com/josh/eurekaserviceapp/EurekaServiceAppApplication.java
@@ -0,0 +1,15 @@
+package com.josh.eurekaserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceAppApplication.class, args);
+	}
+
+}
